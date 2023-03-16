d0d9d37cee9e85e19f6386830e7526209bb98184
Rwin <rwinzhao@gmail.com>
2023-03-17 09:48:25 +1300

    change production

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql')
 const { schema } = require('./schema/schema')
@@ -6,9 +7,9 @@ const colors = require('colors')
 const connectDB = require('./config/db')
 const cors = require('cors')
 
-app.use(cors())
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(cors())
 connectDB()
 
 
@@ -16,5 +17,16 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development'
 }))
+
+// Serve frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => res.sendFile(
+        path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+    ));
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.listen(port, console.log(`Server running on ${port}`))
